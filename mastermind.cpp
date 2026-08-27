a45cd5ec5c4515682d84ca2fdd6dcf2a470a7bab
Louis Gauvreau <e.lgauvreau2@etu.cvm.qc.ca>
2026-08-27 17:51:13 -0400

    mastermind

---
+++ mastermind.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+const char LETTRES_VALIDES{ 'q', 'w', 'e', 'r', 't', 'y' };
+
+char reponseJoueur[5]{};
+
+char enige[5]{};
+
+char copieEnigme[5], {};
+
+void genererEnignme()
+{
+
+}
+bool lettreValide(char lettre)
+{
+
+}
+
+int	main()
+{
+	return 0;
+}
